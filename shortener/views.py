78a88c018bdde331ac203afb68f75ffdad8702d3
Tasnuvaleeya <tasnuva2606@gmail.com>
2017-08-01 11:25:58 +0600

    added 'Class based' and 'Function based view'

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
 # Create your views here.
+
+
+def kirr_redirect_view(request, *args, **kwargs):    # function based view
+    return HttpResponse('hello')
+
+
+class KirrCBView(View):                              # class based view
+    def get(self, request, *args,**kwargs):
+        return HttpResponse('hello again')
